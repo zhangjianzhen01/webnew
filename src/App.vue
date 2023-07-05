24fbc57934457802f53b8930348a191feb90837f
zhangjianzhen01 <124220911+zhangjianzhen01@users.noreply.github.com>
2023-07-05 19:16:45 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
+<!-- App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-link v-show="shouldShowSearchLink" @click="goToSearch">查询</el-link>
+    <el-link v-show="shouldShowBackLink" @click="goToHome">返回</el-link>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    shouldShowSearchLink() {
+      return this.$route.path !== '/search';
+    },
+    shouldShowBackLink() {
+      return this.$route.path !== '/';
+    }
+  },
+  methods: {
+    goToSearch() {
+      this.$router.push('/search');
+    },
+    goToHome() {
+      this.$router.push('/');
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
+// main.js
 import { createApp } from 'vue'
 import App from './App.vue'
+import SerialNumberList from './assets/SerialNumberList.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/search',
+        component: SerialNumberList
+    },
+    {
+        path: '/',
+        component: App
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
