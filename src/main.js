27819854893388a1a1af1bd96bca7ecc66559d38
zhangjianzhen01 <124220911+zhangjianzhen01@users.noreply.github.com>
2023-07-07 16:32:42 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {createApp} from 'vue';
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHistory} from "vue-router";
 import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
